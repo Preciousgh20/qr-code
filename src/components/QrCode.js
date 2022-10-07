c1749d5c43a8d9203bfb668367a1fd391784b8f3
Asare Patience Darkowaa <patiencedarkowaa21@gmail.com>
2022-10-06 18:14:48 -0700

    clean up

--- src/components/QrCode.js
+++ src/components/QrCode.js
@@ -5,7 +5,7 @@ const QrCode = () => {
     return <>
         <div className="bg-indigo-100 h-screen flex items-center justify-center">
             <div className="p-2 bg-white w-52 rounded-xl shadow-lg">
-                <img src={qrimage} alt="img" className="w-full h-48 object-cover rounded-md " />
+                <img src={qrimage} alt="img" className="w-full h-48 object-cover rounded-md "/>
                 <div className="my-3 text-center">
                     <h1 className=" px-2 font-bold text-sm">Improve your front-end skills by building projects</h1>
                     <p className="text-xs mt-2 text-gray-400">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
